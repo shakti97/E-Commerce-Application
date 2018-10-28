3d6d1ef778cefaa5ac8e50681fa3ae8f10084ce7
shakti97 <shaktisingh230797@gmail.com>
2018-10-28 15:41:36 +0530

    adding the user Component

--- src/app/user/website/products/products.component.ts
+++ src/app/user/website/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import CrudProductService from 'Services/CrudProduct.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products : any;
+  constructor(private crudProduct : CrudProductService) {
+    this.products=[];
+   }
 
   ngOnInit() {
+    let showProducts=this.crudProduct.ShowProduct();
+            showProducts.then((Data)=>{
+                console.log("Data",Data);
+                this.products=Data.products;
+            })
   }
 
 }
